2936026b0cae45d44f65f741e25ab7c842eb50a8
Andrés Muñoz <andres.munoz@idukay.com>
2021-10-31 18:45:52 -0500

    return to origin

--- src/App/index.js
+++ src/App/index.js
@@ -1,60 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
+
 function App() {
-  const [test, setTest] =  useState('stateFromApp')
-  
   return (
-    <>
-      <TodoHeader>
-        <TodoCounter test={test}></TodoCounter>
-        <TodoSearch test={test}></TodoSearch>
-      </TodoHeader>
-      <TodoList>
-        <TodoItem test={test}></TodoItem>
-      </TodoList>
-    </>
-  );
-}
-
-function TodoHeader({children}) {
-  return (
-    <header style={{border: "1px solid green"}}>
-      {children}
-    </header>
-  );
-}
-
-function TodoList({children}) {
-  return (
-    <section className="TodoList-container" style={{border: "1px solid blue"}}>
-      {children}
-    </section>
-  );
-}
-
-function TodoCounter({test}) {
-  return (
-    <div className="TodoCounter-container">
-      <p>TodoCounter {test}</p>
-    </div>
-  );
-}
-
-function TodoSearch({test}) {
-  return (
-    <div className="TodoSearch-container">
-      <p>TodoSearch {test}</p>
-    </div>
-  );
-}
-
-function TodoItem({test}) {
-  return (
-    <div className="TodoItem-container">
-      <p>TodoItem {test}</p>
-    </div>
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   );
 }
 
